cd01633f19e42fe631f7a37ea52e374a7aec4712
Ross Hill <rosshill88@gmail.com>
2018-02-19 22:07:30 +0000

    Working in browser, displays images

--- public/app.js
+++ public/app.js
@@ -1,31 +1,35 @@
 let app = function() {
 
-const CATS_ARRAY = [
-  {name: "Boba", favourite_food: "Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
-  {name: "Barnaby", favourite_food: "Tuna", image:"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
-  {name: "Max", favourite_food: "Whiskas Temptations", image:"http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
-]
+  const CATS_ARRAY = [
+    {name: "Boba", favourite_food: "Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+    {name: "Barnaby", favourite_food: "Tuna", image:"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+    {name: "Max", favourite_food: "Whiskas Temptations", image:"http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  ]
 
-let createCatListItem = function() {
-  let catUl = document.createElement('ul');
-  catUl.classlist.add("cat");
-  return catUl;
-}
+  const createCatListItem = function(name, favourite_food, image) {
 
-let createCat = function(name, favourite_food, image) {
-  let nameLi = document.createElement('li');
+  const catUl = document.createElement('ul');
+  catUl.classList.add('cat');
+
+  const nameLi = document.createElement('li');
   nameLi.innerText = "Name: " + name;
-  let foodLi = document.createElement('li');
+  const foodLi = document.createElement('li');
   foodLi.innerText = "Favourite food: " + favourite_food;
-  let imageLi = document.createElement('li');
-  imageLi.innerText = image;
-}
+  const imageLi = document.createElement('li');
+  imageLi.innerHTML = "<img src='" + image + "'/>";
 
 
+  catUl.appendChild(nameLi);
+  catUl.appendChild(foodLi);
+  catUl.appendChild(imageLi);
 
+  const cats = document.getElementById('cats');
+  cats.appendChild(catUl);
+  }
 
-
-
+  for (const cat of CATS_ARRAY) {
+    createCatListItem(cat.name, cat.favourite_food, cat.image);
+  };
 }
 
 document.addEventListener("DOMContentLoaded", app);
