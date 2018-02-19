7defbf88db635fa97c07e88d925293306757644a
Ross Hill <rosshill88@gmail.com>
2018-02-19 20:24:30 +0000

    Created cats array

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 let app = function() {
 
 const CATS_ARRAY = [
-  {name: "Boba", favourite_food: "Sock fluff", image:"#"},
-  {name: "Barnaby", favourite_food: "Tuna", image:"#"},
-  {name: "Max", favourite_food: "Whiskas Temptations", image:"#"},  
+  {name: "Boba", favourite_food: "Sock fluff", image:"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", favourite_food: "Tuna", image:"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: "Max", favourite_food: "Whiskas Temptations", image:"http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},  
 ]
 
 
