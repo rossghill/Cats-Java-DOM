f7a1036f2346fdb894ead97633f26d8c92c66be1
Ross Hill <rosshill88@gmail.com>
2018-02-19 20:20:43 +0000

    initial commit

---
+++ public/app.js
@@ -0,0 +1,18 @@
+let app = function() {
+
+const CATS_ARRAY = [
+  {name: "Boba", favourite_food: "Sock fluff", image:"#"},
+  {name: "Barnaby", favourite_food: "Tuna", image:"#"},
+  {name: "Max", favourite_food: "Whiskas Temptations", image:"#"},  
+]
+
+
+
+
+
+
+
+
+}
+
+document.addEventListener("DOMContentLoaded", app);
